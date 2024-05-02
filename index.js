fdb5c690c3d1996c38339d3689f47ac8ae46c3ac
Vlad Anisov <anisovlad@outlook.com>
2024-05-02 00:53:53 -0700

    chore: ready for npm packaging

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+#! /usr/bin/env node
 const { Command } = require("commander");
-const program = new Command();
 const checkWebsite = require("./commands/web-status.command");
+
+const program = new Command();
 
 program.name("web-status").usage("[options]").description("Check the status of a website.").version("1.0.0");
 
